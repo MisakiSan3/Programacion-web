5afc7b8449980d6b913dc377351270b11456d629
MisakiSan3 <iot.gomez@yavirac.edu.ec>
2022-12-15 16:03:53 -0500

    Angular

--- Angular/client/src/app/app.module.ts
+++ Angular/client/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { FooterComponent } from './shared/footer/footer.component';
 import { PagesComponent } from './pages/pages.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { ProductComponent } from './pages/product/product.component';
 import { CategoryComponent } from './pages/category/category.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
@@ -14,7 +15,7 @@ import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component
 import { NoFoundPagesComponent } from './no-found-pages.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent, PagesComponent, LoginComponent, RegisterComponent, ProductComponent, CategoryComponent, SidebarComponent, BreadcrumbsComponent, NoFoundPagesComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, PagesComponent, LoginComponent, RegisterComponent,DashboardComponent, ProductComponent, CategoryComponent, SidebarComponent, BreadcrumbsComponent, NoFoundPagesComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
