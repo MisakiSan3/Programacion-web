1258bc8a145ae73ca762e1356070b868c242d6ed
tz <tzcristhiangt90@gmail.com>
2022-12-14 20:04:06 -0500

    Proyecto-angular

--- Angular/client/src/app/app.module.ts
+++ Angular/client/src/app/app.module.ts
@@ -3,12 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
-import { NavComponent } from './shared/nav/nav.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { PagesComponent } from './pages/pages.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { ProductComponent } from './pages/product/product.component';
+import { CategoryComponent } from './pages/category/category.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { NoFoundPagesComponent } from './no-found-pages.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, NavComponent, FooterComponent, PagesComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, PagesComponent, LoginComponent, RegisterComponent, ProductComponent, CategoryComponent, SidebarComponent, BreadcrumbsComponent, NoFoundPagesComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
