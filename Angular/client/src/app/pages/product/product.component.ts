a4359fa0de5d5d3706b08b76749c629295fb5a4f
MisakiSan3 <you@example.com>
2023-05-09 19:23:20 -0500

    Ruteo principal y secundario

--- Angular/client/src/app/pages/product/product.component.ts
+++ Angular/client/src/app/pages/product/product.component.ts
@@ -1,7 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+
 import { Component, inject, OnInit } from '@angular/core';
-import { ProductModel,updateProductModel } from 'src/app/models/product.model';
-import { ProductService } from 'src/app/services/product.service';
+
 
 @Component({
   selector: 'app-product',
@@ -9,62 +8,8 @@ import { ProductService } from 'src/app/services/product.service';
   styleUrls: ['./product.component.css'],
 })
 export class ProductComponent implements OnInit {
-  private productService = inject(ProductService);
-  productList: ProductModel[] = [];
-  selectedProduct: updateProductModel = {id: 0, title:"", description:"",price:0, images:[], category: {id:0,name:""}}
-
-  constructor() {}
-
-  data = {
-    title: 'Libros',
-    price: 14,
-    description: 'Halo Image / Isak Gómez',
-    images: [
-      'https://sm.ign.com/ign_es/screenshot/default/analisis-halo-infinite_cjdd.jpg',
-    ],
-    categoryId: 1,
-  };
-
+  
   ngOnInit(): void {
-    this.getProducts();
-    //this.productService.getProduct();
-    //this.productService.createProduct();
-    //this.productService.updateProduct();
-  }
-  async getProducts(){
-    const response = this.productService.getAll().subscribe((response) => {
-      this.productList = response;
-      console.log(response);
-    });
-  }
-  getProduct() {
-    const response = this.productService.getOne(7).subscribe((response) => {
-      console.log(response);
-    });
-  }
-  createProduct() {
-    const response = this.productService
-      .store(this.data)
-      .subscribe((response) => {
-        console.log(response);
-      });
-  }
-  updateProduct(product: updateProductModel) {
-    const response = this.productService
-      .update(product.id, product)
-      .subscribe((response) => {
-        console.log(response);
-      });
-      this.getProducts()
-  }
-  deleteProduct(id: number) {
-    const response = this.productService.destroy(id).subscribe((response) => {
-      this.productList = this.productList.filter(product => product.id != id)
-      console.log(response);
-    });
-  }
-  editProduct(product: ProductModel){
-    this.selectedProduct = product;
   }
 
 }
